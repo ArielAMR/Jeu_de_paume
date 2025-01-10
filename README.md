6d0d0e91f2e873ff9ed6a4f252bbd98c98292dd6
ariel <ariel.amriou@epitech.eu>
2025-01-10 01:35:48 +0100

    feat: add of argv to the calculation

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Jeu_de_paume
+\
+Jeu de paume is an early form of tennis, originating in France in the 12th century. It was originally played with the palm of the hand, before evolving to use rackets. The game is considered a precursor to modern tennis.\
+\
+This program calculate the range of angle you need to inclinate you'r racket to put the ball in the back net of the opponent according to the speed of the ball, the height of the racket and the distance from you to the back net.\
+The measures are based on Fontainbleau jeu de paume court.\
+\
+USAGE\
+- type "./jdp.py -h" to understand the arguments 
--- jdp.py
+++ jdp.py
@@ -1,21 +1,45 @@
-import math
+##!/usr/bin/env python 
+##
+## EPITECH PROJECT, 2024
+## Jeu_de_paume
+## File description:
+## jdp.py
+##
+
+
+from math import *
+from sys import *
+
 g = 3.71
 def calculateur_teta(V0, D, H, h):
     teta_1 = 0
-    teta_2 = math.pi / 2
+    teta_2 = pi / 2
     teta = (teta_1 + teta_2) / 2
-    y =  - (g * D**2) / (2 * V0**2 * math.cos(teta)**2) + D * math.tan(teta) + H
+    y =  - (g * D**2) / (2 * V0**2 * cos(teta)**2) + D * tan(teta) + H
     while abs(y - h) > 0.01:
         if y > h:
             teta_2 = teta
         else:
             teta_1 = teta
         teta = (teta_1 + teta_2) / 2
-        y =  - (g * D**2) / (2 * V0**2 * math.cos(teta)**2) + D * math.tan(teta) + H
-    print(math.degrees(teta))
+        y =  - (g * D**2) / (2 * V0**2 * cos(teta)**2) + D * tan(teta) + H
+    print(f"{degrees(teta):.3f}°")
 def calculateur_i(V0, D, H):
-    Ay2 = 1.1 + 0.03 
+    Ay2 = 1.1 + 0.03
     calculateur_teta(V0, D, H, Ay2)
     By2 = 2.2 - 0.03
     calculateur_teta(V0, D, H, By2)
-calculateur_i(18.6, 28, 1)
+
+if len(argv) == 2:
+    print("Give 3 arguments. 1st is the original speed, 2nd is the distance to the back net, and 3d is the height racket")
+    exit(0)
+
+if len(argv) != 4:
+    print("please insert 3 arguments or retry with -h.")
+    exit(84)
+
+for i in range(3):
+    if argv[i + 1].replace(".", "").isnumeric() == False:
+        exit(84)
+
+calculateur_i(float(argv[1]), float(argv[2]), float(argv[3]))
